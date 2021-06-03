7f56992f5dbeafa153fe1225547b8587efbb937a
Phileas Condemine <phileas.condemine@gmail.com>
2021-06-03 18:27:10 +0200

    update 2021-06-03

--- final_touch.R
+++ final_touch.R
@@ -6,7 +6,7 @@ unlink("docs",T,T)
 shell("rename _site docs")# rename directory _site/ to docs
 # file.copy("index.Rmd","readme.md",overwrite = T)# copy index.Rmd (CV) to readme.md
 file.rename("index.pdf","cv_en_Phileas_Condemine.pdf")# copy index.Rmd (CV) to readme.md
-
+file.copy("docs/cv_en_Phileas_Condemine.pdf","cv_en_Phileas_Condemine.pdf",overwrite = T)
 
 system('git config --global user.email "phileas.condemine@gmail.com"')
 system('git config --global user.name "Phileas Condemine"')
