d1eb5012da777c464287af97230abc8a0d539f79
Phileas Condemine <phileas.condemine@gmail.com>
2022-05-24 19:30:05 +0200

    update 2022-05-24

--- final_touch.R
+++ final_touch.R
@@ -1,11 +1,12 @@
 usethis::use_mit_license("Phileas Condemine")
 
-unlink("docs",T,T)
+# unlink("docs",T,T)
+rmarkdown::clean_site(preview = FALSE)
 # rmarkdown::render("index.Rmd",output_format = "pagedown::html_resume")#then the right html
 rmarkdown::render_site()# bizarre, ça ne crée plus le pdf, il faut aller dans index.Rmd et cliquer sur knit pour que le chrome_print fasse effet...
 # shell("rename _site docs")# rename directory _site/ to docs
 # file.copy("index.Rmd","readme.md",overwrite = T)# copy index.Rmd (CV) to readme.md
-file.rename("index.pdf","cv_en_Phileas_Condemine.pdf")# copy index.Rmd (CV) to readme.md
+# file.rename("index.pdf","cv_en_Phileas_Condemine.pdf")# copy index.Rmd (CV) to readme.md
 # file.copy("docs/cv_en_Phileas_Condemine.pdf","cv_en_Phileas_Condemine.pdf",overwrite = T)
 
 system('git config --global user.email "phileas.condemine@gmail.com"')
@@ -14,3 +15,9 @@ system('git config --global user.name "Phileas Condemine"')
 system("git add .")
 system(sprintf('git commit -m "update %s"',Sys.Date()))
 system("git push origin master")
+
+print("
+to publish the website on netim (phileascondemine.com),
+open filezilla, connect to the hosting server,
+copy the docs/ files in the public_www/ folder on the remote.
+      ")
